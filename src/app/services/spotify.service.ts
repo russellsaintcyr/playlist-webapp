0a716e154fbcf434f1613142b298aa40a5aeffd2
Russell Saint Cyr <russtcyr@yahoo.com>
2017-10-18 09:29:00 -0400

    Working spotify!!!

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -8,8 +8,21 @@ export class SpotifyService {
 
   private searchURL: string;
   private authURL: string;
+  private userID: string;
+  private bearerToken: string;
 
   constructor(private _http: Http) {
+    this.userID = 'x1111x';
+    this.bearerToken = 'BQAmM4DT60KF1574kJsZWHp-bx0mStR3w2PAu30-AHyiCtkz75LlM73Gs5f7fZV41DiBe6YQaOhmESSNB4G-m9d9UDgtWCMUDMWZU-LImGxo9W35jzVifnHlF5cbCEbQzGmZOmt2oAVNf-ibZD82pE6WV2xBgCg9EpjExnDBSzXf4EMTXjcmkWTo0cLuoaSp1Q2RWArck26muyRuKKh5vpgxKUI6HABuTj1TLemkN0xRH2Z-V0IAs_1bwDmCRq3_8o9KlmZhwM8S8Fj1Gu7iIw'
+  }
+
+  getPlaylist(playlistID: string) {
+    // spotify:user:x1111x:playlist:46JHZX9X1hHUpxhZCkKuS1
+    let spotURL = 'https://api.spotify.com/v1/users/' + this.userID + '/playlists/' + playlistID + '/tracks';
+    console.log(spotURL);
+    return this._http.get(spotURL, {headers: new Headers({
+      'Authorization': 'Bearer '+ this.bearerToken
+    })}).map(res => res.json())
   }
 
   searchMusic(str: string, type = 'artist') {
