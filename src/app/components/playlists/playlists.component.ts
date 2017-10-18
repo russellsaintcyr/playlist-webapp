f27f9f301692c8fe8746b68ff9bf02472ce0eb77
Russell Saint Cyr <rcyr@creditshop.com>
2017-10-18 09:56:13 -0400

    Got all track JSON data from spotify for 2017 playlist. Used local storage and offset.

--- src/app/components/playlists/playlists.component.ts
+++ src/app/components/playlists/playlists.component.ts
@@ -11,7 +11,7 @@ export class PlaylistsComponent implements OnInit {
 
   private  priv: string;
   public pub: string;
-  private tracks: Object;
+  public tracks: Object;
 
   constructor(private _spotifyService:SpotifyService) {
   this.priv = 'Privy';
@@ -21,11 +21,12 @@ export class PlaylistsComponent implements OnInit {
   ngOnInit() {
   }
 
-  showPlaylist(event) {
-    console.log(event);
-    this._spotifyService.getPlaylist('46JHZX9X1hHUpxhZCkKuS1').subscribe(res => {
+  showPlaylist(offset) {
+    // console.log(event);
+    this._spotifyService.getPlaylist('46JHZX9X1hHUpxhZCkKuS1', offset).subscribe(res => {
         console.log(res);
         this.tracks = res.items;
+        localStorage.setItem('tracks-' + offset, JSON.stringify(res.items));
       },
       err => {
         console.log('Error: ' + err.statusText);
--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,12 +13,12 @@ export class SpotifyService {
 
   constructor(private _http: Http) {
     this.userID = 'x1111x';
-    this.bearerToken = 'BQAmM4DT60KF1574kJsZWHp-bx0mStR3w2PAu30-AHyiCtkz75LlM73Gs5f7fZV41DiBe6YQaOhmESSNB4G-m9d9UDgtWCMUDMWZU-LImGxo9W35jzVifnHlF5cbCEbQzGmZOmt2oAVNf-ibZD82pE6WV2xBgCg9EpjExnDBSzXf4EMTXjcmkWTo0cLuoaSp1Q2RWArck26muyRuKKh5vpgxKUI6HABuTj1TLemkN0xRH2Z-V0IAs_1bwDmCRq3_8o9KlmZhwM8S8Fj1Gu7iIw'
+    this.bearerToken = 'BQBZdSdEliBX3AOIrqbvyvoTqYLrFsJ3N2JFDzWkPrBstZVHYR1z6T2ZoQQXP5RpNddvBjX_Yfp7SEpUC-2EkKBfuG3TEvS64vgFCKv5hHKyQFTTvBB6JXdBan7486-KAK0kQ66JT9Fv9m4IHK7_0i1JMC4s5UF6auiM75etVu-jz5g2gAt1RNlh0xYz-vGR8Z97b-vPIme5XcTX3qEOI-QdHQTK_Bhh1vErNmWupI8TunHuCMfKcDYsA9oTN51NOF0EK2j23Ce4VsnpaWe1dw'
   }
 
-  getPlaylist(playlistID: string) {
+  getPlaylist(playlistID: string, offset: number) {
     // spotify:user:x1111x:playlist:46JHZX9X1hHUpxhZCkKuS1
-    let spotURL = 'https://api.spotify.com/v1/users/' + this.userID + '/playlists/' + playlistID + '/tracks';
+    let spotURL = 'https://api.spotify.com/v1/users/' + this.userID + '/playlists/' + playlistID + '/tracks?offset=' + offset;
     console.log(spotURL);
     return this._http.get(spotURL, {headers: new Headers({
       'Authorization': 'Bearer '+ this.bearerToken
