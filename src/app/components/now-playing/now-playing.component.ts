1717a6a9e053b6fc7d82b60c2cbae6fe975f11cc
Russell Saint Cyr <rcyr@creditshop.com>
2017-11-27 08:09:25 -0500

    Adding array find support

--- src/app/components/now-playing/now-playing.component.ts
+++ src/app/components/now-playing/now-playing.component.ts
@@ -30,6 +30,30 @@ export class NowPlayingComponent implements OnInit {
       this.alertService.info('Loaded ' + this.ratings.length + ' ratings from local data.');
     }
 
+    if (!Array.prototype.find) {
+      console.log('Adding Array.find support');
+      Array.prototype.find = function(predicate) {
+        if (this == null) {
+          throw new TypeError('Array.prototype.find called on null or undefined');
+        }
+        if (typeof predicate !== 'function') {
+          throw new TypeError('predicate must be a function');
+        }
+        var list = Object(this);
+        var length = list.length >>> 0;
+        var thisArg = arguments[1];
+        var value;
+
+        for (var i = 0; i < length; i++) {
+          value = list[i];
+          if (predicate.call(thisArg, value, i, list)) {
+            return value;
+          }
+        }
+        return undefined;
+      };
+    }
+
   }
 
   getCurrentlyPlaying(intervalId) {
@@ -51,6 +75,9 @@ export class NowPlayingComponent implements OnInit {
             });
           } else {
             this.alertService.warn('Array.find not supported.')
+            let obj = this.ratings.find(function (obj: Rating) {
+              return obj.trackURI === res.item.uri;
+            });
           }
           if (obj === undefined) {
             // reset stars if no rating
